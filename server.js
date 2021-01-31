efbb2ec738a7f9558661a3518971d8279cf4e1f8
Logan Morro <loganfmorro@gmail.com>
2021-01-31 14:21:28 -0500

    added routes on server

--- server.js
+++ server.js
@@ -20,3 +20,10 @@ mongoose.connect(
         useFindAndModify: false
     }
 );
+
+//here is where we apply our routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    console.log('App running on port ${PORT}!');
+});
