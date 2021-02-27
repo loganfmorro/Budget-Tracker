4248d91da0a5a4354e28119d9578af72187f1cc7
Logan Morro <loganfmorro@gmail.com>
2021-02-26 23:11:07 -0500

    updating packagejson

--- server.js
+++ server.js
@@ -25,5 +25,6 @@ mongoose.connect(
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log('App running on port ${PORT}!');
+    console.log(`App running on port ${PORT}!`);
 });
+
